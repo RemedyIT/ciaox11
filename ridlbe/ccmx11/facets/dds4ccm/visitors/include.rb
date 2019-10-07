2533b6761433207edb8e6363c039f6f4e9f717e1
Johnny Willemsen <jwillemsen@remedy.nl>
2019-10-07 15:23:34 +0200

    In addition to ccm_dds.idl also Components.idl has to be ignored to include the ddsx11 typesupport related idl
        * ridlbe/ccmx11/facets/dds4ccm/visitors/include.rb:

--- ridlbe/ccmx11/facets/dds4ccm/visitors/include.rb
+++ ridlbe/ccmx11/facets/dds4ccm/visitors/include.rb
@@ -12,14 +12,13 @@ require 'ridlbe/c++11/visitors/include'
 module IDL
   module Cxx11
     # Reopen visitor class to add dds include specifics
-    #
     class IncludeVisitor
       def ddsx11_typesupport_include?
-        File.basename(node.filename) != 'ccm_dds.idl'
+        !%w{ccm_dds.idl Components.idl}.include?(File.basename(node.filename))
       end
 
       def ddsx11_typesupport_include
-        self.include_directory + '/' + File.basename(node.filename, '.*')+params[:dds_typesupport_pfx]+params[:hdr_ext]
+        self.include_directory + '/' + File.basename(node.filename, '.*') + params[:dds_typesupport_pfx] + params[:hdr_ext]
       end
     end
   end
