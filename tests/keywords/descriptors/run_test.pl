cc599ef7ae8de5af7f6e76f6d132014ec494eb53
Johnny Willemsen <jwillemsen@remedy.nl>
2020-03-06 18:35:51 +0100

    Add idl tests to the lst files
        * connectors/ami4ccm/tests/idl_test/descriptors/run_test.pl:
        * connectors/corba4ccm/tests/idl_tests/derived/descriptors/run_test.pl:
        * connectors/corba4ccm/tests/idl_tests/multi_modules/descriptors/run_test.pl:
        * tests/keywords/descriptors/run_test.pl:
        * tests/kitchensink/descriptors/run_test.pl:
          Added, simpy noop tests but this way we can add them to the lst file
    
        * bin/ciaox11_tests.lst:
          Add all idl tests so that they are compiled with a brix11 run list
    
        * ridlbe/ccmx11/templates/lem_idl/component_lem.erb:
          Use unescaped interface name when generating IDL

---
+++ tests/keywords/descriptors/run_test.pl
@@ -0,0 +1,13 @@
+#---------------------------------------------------------------------
+# @file   run_test.pl
+# @author Johnny Willemsen
+#
+# @copyright Copyright (c) Remedy IT Expertise BV
+#---------------------------------------------------------------------
+eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
+    & eval 'exec perl -S $0 $argv:q'
+    if 0;
+
+# -*- perl -*-
+
+exit 0;
