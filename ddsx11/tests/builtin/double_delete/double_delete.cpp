59d7be87b5dae9b21a3ef322073790b311be7ee0
Johnny Willemsen <jwillemsen@remedy.nl>
2021-07-08 19:09:56 +0200

    Check for the same
        * ddsx11/tests/builtin/double_delete/double_delete.cpp:

--- ddsx11/tests/builtin/double_delete/double_delete.cpp
+++ ddsx11/tests/builtin/double_delete/double_delete.cpp
@@ -72,11 +72,11 @@ int main (int, char *[])
       DDS::Duration_t const zero {0, 0};
       DDS::traits<ShapeType>::topic_ref_type topic2 = domain_participant->find_topic ("Square", zero);
 
-      // topic2 and topic should have different instance handles
-      if (topic2->get_instance_handle () == topic->get_instance_handle ())
+      // topic2 and topic should have the same instance handles
+      if (topic2->get_instance_handle () != topic->get_instance_handle ())
       {
-        DDSX11_TEST_ERROR << "double_delete: topic and topic2 should have different instance handles "
-                          << topic2->get_instance_handle ()
+        DDSX11_TEST_ERROR << "double_delete: topic and topic2 should have the same instance handles "
+                          << topic2->get_instance_handle () << ":"
                           << topic->get_instance_handle ()
                           << std::endl;
         return 1;
