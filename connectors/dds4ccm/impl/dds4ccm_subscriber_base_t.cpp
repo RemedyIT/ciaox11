0e40e9402266fbbb7a5923c7181d7af40a80f1f0
Johnny Willemsen <jwillemsen@remedy.nl>
2022-05-17 09:03:53 +0200

    Fixed typo in comment
        * connectors/dds4ccm/impl/dds4ccm_subscriber_base_t.cpp:

--- connectors/dds4ccm/impl/dds4ccm_subscriber_base_t.cpp
+++ connectors/dds4ccm/impl/dds4ccm_subscriber_base_t.cpp
@@ -83,7 +83,7 @@ DDS_Subscriber_Base_T<CCM_TYPE, TOPIC_TYPE, TOPIC_SEQ_TYPE>::configuration_compl
     }
     else
     {
-      // To create a datareader, the qos should be passed. In order o pass a
+      // To create a datareader, the qos should be passed. In order to pass a
       // valid qos we first retrieve it from DDS itself.
       ::DDS::DataReaderQos drqos;
       ::DDS::ReturnCode_t const retcode =
