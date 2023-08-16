4561d5ee8a7aba290dce4042927c8b46ca7dea98
Johnny Willemsen <jwillemsen@remedy.nl>
2023-08-16 11:41:41 +0200

    Removed redundant typedef
        * ciaox11/deployment/handlers/ciaox11_state.h:

--- ciaox11/deployment/handlers/ciaox11_state.h
+++ ciaox11/deployment/handlers/ciaox11_state.h
@@ -80,7 +80,6 @@ namespace CIAOX11
     std::mutex state_mutex_;
 
     /// Container administration
-    using CONTAINERS_PAIR = std::pair <std::string, std::shared_ptr<CIAOX11::Container>>;
     using CONTAINERS = std::map <std::string, std::shared_ptr<CIAOX11::Container>>;
 
     CONTAINERS containers_;
@@ -88,16 +87,16 @@ namespace CIAOX11
     /// Administration which components are instantiated in which container
     using INSTANCE_CONTAINER = std::map <std::string, std::string>;
 
-    /// maps instance ids to containers.
+    /// Maps instance ids to containers.
     INSTANCE_CONTAINER instance_container_;
 
     /// Administers configuration of instances
     using INSTANCE_CONFIG = std::map <std::string, Components::ConfigValues>;
 
-    /// maps container ids to configuration.
+    /// Maps container ids to configuration.
     INSTANCE_CONFIG container_config_;
 
-    /// maps instance ids to configuration.
+    /// Maps instance ids to configuration.
     INSTANCE_CONFIG instance_config_;
   };
 
