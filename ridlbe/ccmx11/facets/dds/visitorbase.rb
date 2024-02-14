53ade09f3ed4581c381df1aacc629a8615da6bcf
Johnny Willemsen <jwillemsen@remedy.nl>
2024-02-14 10:36:00 +0100

    Support top_level as possible annotation
        * connectors/dds4ccm/tests/unkeyed_writer/data/unkeyed_writer.idl:
        * ridlbe/ccmx11/facets/dds/visitorbase.rb:
        * ridlbe/ccmx11/facets/dds/writers/ddstypedentitiesidl.rb:
        * ridlbe/ccmx11/facets/dds/writers/ddstypesupport.rb:
        * ridlbe/ccmx11/facets/dds4ccm/writers/dds4ccm_sev_idl.rb:

--- ridlbe/ccmx11/facets/dds/visitorbase.rb
+++ ridlbe/ccmx11/facets/dds/visitorbase.rb
@@ -57,7 +57,7 @@ module IDL
 
       def has_toplevel_annotation?
         # 20190730 Add support for AXCIOMA 2 top-level annotation, issue #4729
-        annot = node.annotations[:'top-level'].first || node.annotations[:TopLevel].first
+        annot = node.annotations[:'top-level'].first || node.annotations[:TopLevel].first || node.annotations[:'top_level'].first
         return false if annot.nil?
 
         annot.fields[:value].nil? || annot.fields[:value]
